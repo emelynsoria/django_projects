23ca55dcc60ede765f8a1d6709d4e082b1f55711
Emelyn Casaul Soria <emelyn.soria@boom.camp>
2020-01-09 17:39:52 +0800

    Added pagination to 'blog_project' project

--- blog_project/blog/views.py
+++ blog_project/blog/views.py
@@ -1,8 +1,15 @@
-# from django.http import HttpResponse
 from django.shortcuts import render
+from django.views.generic import (
+    ListView,
+    DetailView,
+    CreateView,
+    UpdateView,
+    DeleteView,
+)
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from .models import Post
 
-# # dummy data
+# # dummy data to be used in posts
 # post_list = [
 #     {
 #         "author": "CoreyMS",
@@ -20,15 +27,64 @@ from .models import Post
 
 
 def home(request):
-    # return HttpResponse("<h1>Blog Home</h1>")
-
-    # # 'posts' as key for post_list list, w/c will be accessible in the template
+    # # 'posts' as key for 'post_list' list, w/c will be accessible in the template
     # context = {"posts": post_list}
+
     context = {"posts": Post.objects.all()}
     return render(request, "blog/index.html", context)
 
 
+class PostListView(ListView):
+    model = Post
+
+    # will look for <app>/<model>_<viewtype>.html     # ex: blog/post_list.html
+    template_name = "blog/index.html"
+    context_object_name = "posts"
+    ordering = ["-date_posted"]  # DESC; newest to oldest
+    paginate_by = 2
+
+
+class PostDetailView(DetailView):
+    model = Post
+    # # with or without 'template_name' since it's already linked in index.html
+    # template_name = "blog/post_detail.html"
+
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ["title", "content"]
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ["title", "content"]
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    success_url = "/"
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+
 def about(request):
-    # return HttpResponse("<h1>About Page</h1>")
     return render(request, "blog/about.html", {"title": "About"})
 
