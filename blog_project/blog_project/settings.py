f03282a5f777899388a503938fcbf55d209eff7d
Emelyn Casaul Soria <emelyn.soria@boom.camp>
2020-01-10 10:53:44 +0800

    Updated blog app

--- blog_project/blog_project/settings.py
+++ blog_project/blog_project/settings.py
@@ -132,3 +132,10 @@ LOGIN_REDIRECT_URL = "blog-home"
 # added for redirecting user to 'login' page after going to page not accessible yet
 # ex: http://127.0.0.1:8000/profile/
 LOGIN_URL = "login"
+
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.environ.get("EMAIL_USER")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_PASS")
